1a52f4e587adb9a6f2252eab3a1e0925374f718a
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-08-24 11:39:15 +0100

    added integration test

--- spec/tasks_integration_spec.rb
+++ spec/tasks_integration_spec.rb
@@ -1,5 +1,6 @@
 require "../lib/task_list.rb"
 require "../lib/task.rb"
+require "../lib/task_formatter.rb"
 
 RSpec.describe "tasks integration" do
   it "adds tasks to a list" do
@@ -20,5 +21,18 @@ RSpec.describe "tasks integration" do
     task_1.mark_complete
     task_2.mark_complete
     expect(task_list.all_complete?).to eq true
+  end
+
+  it "adds tasks and returns formatted string" do
+    task_list = TaskList.new
+    task_1 = Task.new("Walk the dog")
+    task_2 = Task.new("Walk the cat")
+    task_list.add(task_1)
+    task_list.add(task_2)
+    task_1.mark_complete
+    task_1_string = TaskFormatter.new(task_1)
+    expect(task_1_string.format).to eq "[x] Walk the dog"
+    task_2_string = TaskFormatter.new(task_2)
+    expect(task_2_string.format).to eq "[ ] Walk the cat"
   end
 end
